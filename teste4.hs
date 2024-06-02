322cf6c800d0250c932a0ba3cb780e1af19a7740
Johnny-LCC <132676210+Johnny-LCC@users.noreply.github.com>
2024-06-02 17:43:19 +0100

    Update teste4.hs

--- teste4.hs
+++ teste4.hs
@@ -1,9 +1,40 @@
 import Cp
-import List
+import List ( cataList, anaList )
+import Nat ( outNat )
 
-len :: [a] -> Int
-len [] = 0
-len (h:t) = 1 + len t
---------------------------------------------------
-maxL = cataList h
-h = either zero maxA where maxA (a,b) = max a b
+l :: [Integer]
+l = [2,1,5,6,2,3]
+
+len :: [a] -> Integer
+len = foldr (\ h -> (+) 1) 0
+
+ld :: Integer -> [Integer]
+ld = anaList ((id -|- split succ id) . outNat)
+-- ld 5 = [5,4,3,2,1] -- ld 3 = [3,2,1] -- ld 0 = []
+
+lista :: [[Integer]]
+lista = [ld x | x <- l]
+
+teste :: [[Integer]] -> [Integer]
+teste = cataList (either nil maxR)
+-- teste lista = [5,5]
+
+maxR :: ([Integer], [Integer]) -> [Integer]
+maxR (h:t, []) = [h] 
+maxR (h:t, l)| head l >= h = if sum l > ((len l)+1) * h then l else h : sub h l 
+             | otherwise = if h > sum l then [h] else head l: l
+
+sub :: Integer -> [Integer] -> [Integer]
+sub _ [] = []
+sub n (h:t) = n : sub n t
+
+-- sum(teste lista) = 10
+
+------------------------------------------------
+-- eu acho que é preciso transformar tudo isto
+-- em apnas um hilomorfismo, do tipo:
+
+--k = hyloList f g
+-- g (ana) :: [a] -> [[a]]
+-- f (cata) :: [[a]] -> a
+-------------------------------------------------
