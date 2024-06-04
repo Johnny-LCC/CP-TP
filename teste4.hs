05d9f9803865aca92d95b2f17167787325e32cfd
Johnny-LCC <132676210+Johnny-LCC@users.noreply.github.com>
2024-06-04 20:37:16 +0100

    Update teste4.hs

--- teste4.hs
+++ teste4.hs
@@ -4,45 +4,30 @@ import Nat ( outNat )
 
 l :: [Integer]
 l = [2,1,5,6,2,3]
-
+--------------------------------------------------------
 len :: [a] -> Integer
 len = foldr (\ h -> (+) 1) 0
-
-ld :: Integer -> [Integer]
-ld = anaList ((id -|- split succ id) . outNat)
--- ld 5 = [5,4,3,2,1] -- ld 3 = [3,2,1] -- ld 0 = []
-
-lista :: [Integer] -> [[Integer]]
-lista l = [ld x | x <- l]
-
-teste :: [[Integer]] -> [Integer]
-teste = cataList (either nil maxR)
--- teste lista = [5,5]
 
 maxR :: ([Integer], [Integer]) -> [Integer]
 maxR ([],h:t) = [h] 
 maxR (h:t, []) = [h] 
 maxR (h:t, l)| head l > h = if sum l > ((len l)+1) * h then singl(sum l) else h : sub h l --
              | head l == h = h:l
-             | otherwise = if h > sum l then h:sub 0 l else head l: l 
+             | otherwise = if h >= (sum l + head l) then h:sub 0 l else head l: l 
 
 sub :: Integer -> [Integer] -> [Integer]
 sub _ [] = []
 sub n (h:t) = n : sub n t
 
-lrh = sum . teste . lista
-
-------------------HILOMORFISMO---------------------
-lrh' = sum . hyloList f g
+lrh = sum . hyloList f g
 f = either nil maxR
-g = (id -|- (cons . split succ singl >< id)) . out
+g = (id -|- ( singl >< id)) . outList
+
+-- Acho que está concluído, mas preciso de realizar
+-- mais alguns testes. Sintam-se livres para fazerem
+-- o mesmo
+---------------------------------------------------
 
 out :: [Integer] -> Either () (Integer, [Integer])
 out [] = i1 ()
 out (h:t) = i2 (h-1,t)
-
--- Acho que o anamorfismo (g) não está perfeito,
--- talvez precise de alterações! 
-
--- A função maxR também está incompleta...
----------------------------------------------------
