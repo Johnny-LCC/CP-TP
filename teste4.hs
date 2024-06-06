0afa0b6720005348fe4093df19b6e9995f4bc1c5
Johnny-LCC <132676210+Johnny-LCC@users.noreply.github.com>
2024-06-06 15:46:34 +0100

    Update teste4.hs

--- teste4.hs
+++ teste4.hs
@@ -8,12 +8,12 @@ l1 = [2,1,5,6,2,3]
 l2 :: [Integer]
 l2 = [3,5,2,1,5,4]
 
-g :: [Integer] -> [[Integer]]
-g = anaList ((id -|- split cons p2) . outList)
-f :: [[Integer]] -> [Integer]
-f = cataList (either nil ff)
-lrh :: [Integer] -> Integer
-lrh = maxCata . f . g
+g = (id -|- split cons p2) . outList
+f = either nil (cons.(mRec >< id))
+lrh = maxCata . hyloList f g
+
+ff = either zero (uncurry max.(mRec >< id))
+lrh' = hyloList ff g
 
 mRec :: [Integer] -> Integer
 mRec [] = 0 --
@@ -25,8 +25,5 @@ auxR (x,[]) = 0
 auxR (x,h:t) | x <= h = 1 + auxR(x,t)
              | otherwise = 0
 
-ff :: ([Integer], [Integer]) -> [Integer]
-ff = cons.(mRec >< id)
-
 maxCata = cataList (either zero (uncurry max))
--- Vou só dar uma ajeitada nos tipos das funções --
+--------------- CONCLUIDO ---------------
